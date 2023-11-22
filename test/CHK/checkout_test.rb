cfca5ea15eb90bd373eef84bd3abaad651c4afba
sbx_user1051 <sbx_user1051@169.254.74.213>
2023-11-22 18:20:09 +0000

    Wed Nov 22 18:20:09 UTC 2023

---
+++ test/CHK/checkout_test.rb
@@ -0,0 +1,30 @@
+# noinspection RubyResolve,RubyResolve
+require_relative '../test_helper'
+require 'logging'
+
+Logging.logger.root.appenders = Logging.appenders.stdout
+
+require_solution 'CHK'
+
+class ClientTest < Minitest::Test
+
+  def test_checkout
+    checkout = Checkout.new
+
+    # Test case 1: Normal pricing
+    assert_equal 50, checkout.checkout("A")
+    assert_equal 115, checkout.checkout("ABCD")
+
+    # Test case 2: Special offers
+    assert_equal 130, checkout.checkout("AAA")
+    assert_equal 45, checkout.checkout("BB")
+
+    # Test case 3: Combination of normal pricing and special offers
+    assert_equal 175, checkout.checkout("AAABB")
+    assert_equal 225, checkout.checkout("AAABBD")
+    #
+    # # Test case 4: Illegal input
+    # assert_equal -1, checkout.checkout("EFG")
+  end
+
+end
