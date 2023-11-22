b2fc8d2446e8db7cb2614cf11013e1d970639758
sbx_user1051 <sbx_user1051@169.254.74.213>
2023-11-22 18:14:09 +0000

    Wed Nov 22 18:14:09 UTC 2023

--- lib/solutions/CHK/checkout.rb
+++ lib/solutions/CHK/checkout.rb
@@ -1,6 +1,15 @@
 # noinspection RubyUnusedLocalVariable
 class Checkout
 
+  def initialize
+    @price_table = {
+      'A' => { price: 50, special_offer: { quantity: 3, offer_price: 130 } },
+      'B' => { price: 30, special_offer: { quantity: 2, offer_price: 45 } },
+      'C' => { price: 20 },
+      'D' => { price: 15 }
+    }
+  end
+
   def checkout(skus)
     -1 if skus.nil?
 
@@ -8,4 +17,5 @@ class Checkout
   end
 
 end
+
 
