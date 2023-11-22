7e3ae8bcf643a46949b1411c1d1a8d1a59bd00d4
sbx_user1051 <sbx_user1051@169.254.74.213>
2023-11-22 20:23:10 +0000

    Wed Nov 22 20:23:10 UTC 2023

--- lib/solutions/CHK/checkout.rb
+++ lib/solutions/CHK/checkout.rb
@@ -2,49 +2,74 @@ class Checkout
 
   def initialize
     @price_table = {
-      'A' => { price: 50, },
-      'B' => { price: 30 },
+      'A' => { price: 50, special_offers: [{ quantity: 3, offer_price: 130 }, { quantity: 5, offer_price: 200 }] },
+      'B' => { price: 30, special_offer: { quantity: 2, offer_price: 45 } },
       'C' => { price: 20 },
       'D' => { price: 15 },
-      'E' => { price: 40 },
-    }
-
-    @discount_types = {
-      :offer_price => { priority: 1 },
-      :free_item => { priority: 2 }
-    }
-
-    @product_discounts = {
-      'A' => { quantity: 3, offer_price: 130 },
-      'B' => { quantity: 2, offer_price: 45 },
-      'E' => { quantity: 2, free_item: 'B' }
+      'E' => { price: 40, special_offer: { quantity: 2, free_item: 'B' } }
     }
   end
 
   def checkout(skus)
-    return -1 if skus.nil?
-
-    item_counts = Hash.new(0)
-    skus.each_char { |item| item_counts[item] += 1 }
-
-    # Remove free items from the item counts if quantity is met
-    @product_discounts.each do |item, product_discount|
-      if product_discount[:free_item]
-        free_item_count = [item_counts[product_discount[:free_item]], item_counts[item] / product_discount[:quantity]].min
-        item_counts[product_discount[:free_item]] -= free_item_count
-      end
-    end
+    return -1 unless skus.is_a?(String)
 
     total_price = 0
-    item_counts.each do |item, count|
+    item_counts = Hash.new(0)
+
+    skus.each_char do |item|
       return -1 unless @price_table.key?(item)
 
-      price_info = @price_table[item]
-      total_price += calculate_item_price(count, price_info, item)
+      item_counts[item] += 1
+    end
+
+    item_counts.each do |item, count|
+      total_price += calculate_item_price(item, count)
+    end
+  end
+
+  private
+
+  def calculate_item_price(item, count)
+    price_info = @price_table[item]
+    special_offers = price_info[:special_offers]
+    special_offer = price_info[:special_offer]
+
+    if special_offers
+      total_price = apply_multi_price_offers(item, count, special_offers)
+    elsif special_offer
+      total_price = apply_special_offer(item, count, special_offer)
+    else
+      total_price = count * price_info[:price]
     end
 
     total_price
   end
 
-  private
+  def apply_multi_price_offers(item, count, offers)
+    total_price = 0
+
+    offers.sort_by { |offer| -offer[:quantity] }.each do |offer|
+      offer_batches = count / offer[:quantity]
+      remaining_items = count % offer[:quantity]
+
+      total_price += offer_batches * offer[:offer_price]
+      count = remaining_items
+    end
+
+    total_price
+  end
+
+  def apply_special_offer(item, count, offer)
+    if item == 'E' && offer[:free_item]
+      free_item_count = [count / offer[:quantity], count].min
+      return (count - free_item_count) * @price_table[item][:price]
+    end
+
+    offer_batches = count / offer[:quantity]
+    remaining_items = count % offer[:quantity]
+
+    offer_batches * offer[:offer_price] + remaining_items * @price_table[item][:price]
+  end
+
 end
+
