7d28f1a6f859f2ad397310b79184e09961c0342a
sbx_user1051 <sbx_user1051@169.254.74.213>
2023-11-22 20:32:10 +0000

    Wed Nov 22 20:32:10 UTC 2023

--- lib/solutions/CHK/checkout.rb
+++ lib/solutions/CHK/checkout.rb
@@ -1,5 +1,4 @@
 class Checkout
-
   def initialize
     @price_table = {
       'A' => { price: 50, special_offers: [{ quantity: 3, offer_price: 130 }, { quantity: 5, offer_price: 200 }] },
@@ -10,13 +9,13 @@ class Checkout
     }
   end
 
-  def checkout(skus)
-    return -1 unless skus.is_a?(String)
+  def checkout(items)
+    return -1 unless items.is_a?(String)
 
     total_price = 0
     item_counts = Hash.new(0)
 
-    skus.each_char do |item|
+    items.each_char do |item|
       return -1 unless @price_table.key?(item)
 
       item_counts[item] += 1
@@ -32,19 +31,15 @@ class Checkout
   private
 
   def calculate_item_price(item, count)
-    puts "Calculating price for #{item} x #{count}"
     price_info = @price_table[item]
     special_offers = price_info[:special_offers]
     special_offer = price_info[:special_offer]
 
     if special_offers
-      puts "Applying multi-price offers"
       total_price = apply_multi_price_offers(item, count, special_offers)
     elsif special_offer
-      puts "Applying special offer"
       total_price = apply_special_offer(item, count, special_offer)
     else
-      puts "Applying normal price"
       total_price = count * price_info[:price]
     end
 
@@ -67,8 +62,11 @@ class Checkout
 
   def apply_special_offer(item, count, offer)
     if item == 'E' && offer[:free_item]
-      free_item_count = [count / offer[:quantity], count].min
-      return (count - free_item_count) * @price_table[item][:price]
+      eligible_items = count - count % offer[:quantity]
+      free_items = [count / offer[:quantity], eligible_items / offer[:quantity]].min
+      remaining_items = count - eligible_items
+
+      return free_items * @price_table[offer[:free_item]][:price] + remaining_items * @price_table[item][:price]
     end
 
     offer_batches = count / offer[:quantity]
@@ -76,7 +74,6 @@ class Checkout
 
     offer_batches * offer[:offer_price] + remaining_items * @price_table[item][:price]
   end
-
 end
 
 
