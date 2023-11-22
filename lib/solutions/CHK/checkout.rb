3ba2aaaadaade36aa3b4127d9dc1ca35ed7b560c
sbx_user1051 <sbx_user1051@169.254.74.213>
2023-11-22 19:56:10 +0000

    Wed Nov 22 19:56:10 UTC 2023

--- lib/solutions/CHK/checkout.rb
+++ lib/solutions/CHK/checkout.rb
@@ -1,17 +1,18 @@
 class Checkout
+
   def initialize
     @price_table = {
-      'A' => { price: 50 },
-      'B' => { price: 30 },
+      'A' => { price: 50, special_offer: { quantity: 3, offer_price: 130 } },
+      'B' => { price: 30, special_offer: { quantity: 2, offer_price: 45 } },
       'C' => { price: 20 },
       'D' => { price: 15 },
-      'E' => { price: 40 }
+      'E' => { price: 40, special_offer: { quantity: 2, free_item: 'B' } }
     }
 
-    @discount_table = {
-      'A' => { type: :offer_price, quantity: 3, price: 130 },
-      'B' => { type: :offer_price, quantity: 2, price: 45 },
-      'E' => { type: :free_item, quantity: 2, free_item: 'B' }
+    #
+    @discount_types = {
+      :offer_price => { priority: 1 },
+      :free_item => { priority: 2 }
     }
   end
 
@@ -21,10 +22,43 @@ class Checkout
     item_counts = Hash.new(0)
     skus.each_char { |item| item_counts[item] += 1 }
 
-    puts item_counts
+    total_price = 0
+    item_counts.each do |item, count|
+      return -1 unless @price_table.key?(item)
+
+      price_info = @price_table[item]
+      total_price += calculate_item_price(count, price_info, item_counts)
+    end
+
+    total_price
   end
 
+  private
+
+  def calculate_item_price(count, price_info, item_counts)
+    return -1 if count < 0
+
+    if price_info[:special_offer]
+      special_offer = price_info[:special_offer]
+      if special_offer[:free_item]
+        special_price_quantity = count / special_offer[:quantity]
+        regular_price_quantity = count % special_offer[:quantity]
+        free_item_count = [item_counts[special_offer[:free_item]], special_price_quantity].min
+        total_price += (special_price_quantity * price_info[:price]) +
+          (regular_price_quantity * price_info[:price]) -
+          (free_item_count * @price_table[special_offer[:free_item]][:price])
+      else
+        special_price_quantity = count / special_offer[:quantity]
+        regular_price_quantity = count % special_offer[:quantity]
+        total_price += (special_price_quantity * special_offer[:offer_price]) +
+          (regular_price_quantity * price_info[:price])
+      end
+    else
+      count * price_info[:price]
+    end
+  end
 end
+
 
 
 
