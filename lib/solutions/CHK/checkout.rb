b19d6cc2bee7354a08f33f904b05002c5dad58d0
sbx_user1051 <sbx_user1051@169.254.68.53>
2023-11-23 00:31:00 +0000

    Thu Nov 23 00:31:00 UTC 2023

--- lib/solutions/CHK/checkout.rb
+++ lib/solutions/CHK/checkout.rb
@@ -1,12 +1,64 @@
 class Checkout
   def initialize
+    ##
+    # +------+-------+------------------------+
+    # | Item | Price | Special offers         |
+    # +------+-------+------------------------+
+    # | A    | 50    | 3A for 130, 5A for 200 |
+    # | B    | 30    | 2B for 45              |
+    # | C    | 20    |                        |
+    # | D    | 15    |                        |
+    # | E    | 40    | 2E get one B free      |
+    # | F    | 10    | 2F get one F free      |
+    # | G    | 20    |                        |
+    # | H    | 10    | 5H for 45, 10H for 80  |
+    # | I    | 35    |                        |
+    # | J    | 60    |                        |
+    # | K    | 80    | 2K for 150             |
+    # | L    | 90    |                        |
+    # | M    | 15    |                        |
+    # | N    | 40    | 3N get one M free      |
+    # | O    | 10    |                        |
+    # | P    | 50    | 5P for 200             |
+    # | Q    | 30    | 3Q for 80              |
+    # | R    | 50    | 3R get one Q free      |
+    # | S    | 30    |                        |
+    # | T    | 20    |                        |
+    # | U    | 40    | 3U get one U free      |
+    # | V    | 50    | 2V for 90, 3V for 130  |
+    # | W    | 20    |                        |
+    # | X    | 90    |                        |
+    # | Y    | 10    |                        |
+    # | Z    | 50    |                        |
+    # +------+-------+------------------------+
+
     @price_table = {
       'A' => { price: 50, special_offers: [{ quantity: 3, offer_price: 130 }, { quantity: 5, offer_price: 200 }] },
       'B' => { price: 30, special_offers: [{ quantity: 2, offer_price: 45 }] },
       'C' => { price: 20 },
       'D' => { price: 15 },
       'E' => { price: 40, special_offers: [{ quantity: 2, free_item: 'B' }] },
-      'F' => { price: 10, special_offers: [{ quantity: 3, free_item: 'F' }] }
+      'F' => { price: 10, special_offers: [{ quantity: 3, free_item: 'F' }] },
+      'G' => { price: 20 },
+      'H' => { price: 10, special_offers: [{ quantity: 5, offer_price: 45 }, { quantity: 10, offer_price: 80 }] },
+      'I' => { price: 35 },
+      'J' => { price: 60 },
+      'K' => { price: 80, special_offers: [{ quantity: 2, offer_price: 150 }] },
+      'L' => { price: 90 },
+      'M' => { price: 15 },
+      'N' => { price: 40, special_offers: [{ quantity: 3, free_item: 'M' }] },
+      'O' => { price: 10 },
+      'P' => { price: 50, special_offers: [{ quantity: 5, offer_price: 200 }] },
+      'Q' => { price: 30, special_offers: [{ quantity: 3, offer_price: 80 }] },
+      'R' => { price: 50, special_offers: [{ quantity: 3, free_item: 'Q' }] },
+      'S' => { price: 30 },
+      'T' => { price: 20 },
+      'U' => { price: 40, special_offers: [{ quantity: 3, free_item: 'U' }] },
+      'V' => { price: 50, special_offers: [{ quantity: 2, offer_price: 90 }, { quantity: 3, offer_price: 130 }] },
+      'W' => { price: 20 },
+      'X' => { price: 90 },
+      'Y' => { price: 10 },
+      'Z' => { price: 50 }
     }
   end
 
